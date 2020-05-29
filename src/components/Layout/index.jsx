307dc72fbaaa537a20b5e59d2584be112a8f31bc
Martin Bajanik <{ID}+{username}@users.noreply.github.com>
2020-05-29 20:40:44 +0200

    Use lazuMount for wizard

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -13,7 +13,7 @@ class Layout extends React.Component {
         </Helmet>
         {children}
         <div className="footer">
-          Created with passion🍻 by <a href="https://twitter.com/_bayotop" target="_blank">bayo</a> and <a href="https://twitter.com/martinmakarsky" target="blank">makma🚀</a>.
+          Created with passion🍻by <a href="https://twitter.com/_bayotop" target="_blank">bayo</a> and <a href="https://twitter.com/martinmakarsky" target="blank">makma 🚀</a>.
         </div>
       </div>
     )
