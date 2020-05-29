5ab61a291eb86e105a7917629d1e7da43f9639e1
Martin Makarsky <martinmakarsky@gmail.com>
2020-05-29 20:30:52 +0200

    Remove hashkeys.

--- src/components/Wizard/index.jsx
+++ src/components/Wizard/index.jsx
@@ -74,24 +74,24 @@ export const Wizard = () => {
 
   return (
     <div className="wizard">
-      <StepWizard isHashEnabled={true}>
-        <Question hashKey={'1'} question={duration} target={{yes: 2, no: 3}} />
-        <Question hashKey={'2'} question={citizenship} target={{yes: 4, no: 5}} />
-        <Question hashKey={'3'} question={residence} target={{yes: 15, no: 2}} />
-        <Question hashKey={'4'} question={return_in_48} target={{yes: 15, no: 16}} />
-        <Question hashKey={'5'} question={foreigner_1} target={{yes: 4, no: 6}} />
-        <Question hashKey={'6'} question={foreigner_2} target={{yes: 4, no: 7}} />
-        <Question hashKey={'7'} question={foreigner_3} target={{yes: 4, no: 8}} />
-        <Question hashKey={'8'} question={foreigner_4} target={{yes: 4, no: 9}} />
-        <Question hashKey={'9'} question={foreigner_5} target={{yes: 4, no: 10}} />
-        <Question hashKey={'10'} question={foreigner_6} target={{yes: 4, no: 11}} />
-        <Question hashKey={'11'} question={foreigner_7} target={{yes: 4, no: 12}} />
-        <Question hashKey={'12'} question={foreigner_8} target={{yes: 4, no: 13}} />
-        <Question hashKey={'13'} question={foreigner_9} target={{yes: 4, no: 14}} />
-        <Question hashKey={'14'} question={foreigner_10} target={{yes: 4, no: 17}} />
-        <Result hashKey={'15'} result={no_limitations} />
-        <Result hashKey={'16'} result={quarantine} />
-        <Result hashKey={'17'} result={unable_to_enter} />
+      <StepWizard >
+        <Question question={duration} target={{yes: 2, no: 3}} />
+        <Question question={citizenship} target={{yes: 4, no: 5}} />
+        <Question question={residence} target={{yes: 15, no: 2}} />
+        <Question question={return_in_48} target={{yes: 15, no: 16}} />
+        <Question question={foreigner_1} target={{yes: 4, no: 6}} />
+        <Question question={foreigner_2} target={{yes: 4, no: 7}} />
+        <Question question={foreigner_3} target={{yes: 4, no: 8}} />
+        <Question question={foreigner_4} target={{yes: 4, no: 9}} />
+        <Question question={foreigner_5} target={{yes: 4, no: 10}} />
+        <Question question={foreigner_6} target={{yes: 4, no: 11}} />
+        <Question question={foreigner_7} target={{yes: 4, no: 12}} />
+        <Question question={foreigner_8} target={{yes: 4, no: 13}} />
+        <Question question={foreigner_9} target={{yes: 4, no: 14}} />
+        <Question question={foreigner_10} target={{yes: 4, no: 17}} />
+        <Result result={no_limitations} />
+        <Result result={quarantine} />
+        <Result result={unable_to_enter} />
       </StepWizard>
     </div>
   )};
