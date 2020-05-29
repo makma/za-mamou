9b3773ac8c085f49f89358a08db7750d8e396181
Martin Makarsky <martinmakarsky@gmail.com>
2020-05-29 22:12:02 +0200

    Update og tags.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -10,10 +10,6 @@ class Layout extends React.Component {
       <div className="layout">
         <Helmet defaultTitle="Blog by John Doe">
           <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-          <meta property="og:title" content="Za mamou - môžem ísť na Slovensko?" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://zamamou.sk/" />
-          <meta property="og:image" content="/favicon.ico" />
         </Helmet>
         {children}
         <div className="footer">
